d7d3f67139f73d6df164a13f1afdb6daad9b34b1
Gavin McDonnell <gmcdonnell2@berklee.edu>
2023-05-11 10:57:27 -0400

    completed documentation

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function sound(src) {
 	this.sound.src = src;
 	this.sound.setAttribute('preload', 'auto');
 	this.sound.setAttribute('controls', 'none');
-	this.sound.classList.add('clip'); //FCC project #6
+	this.sound.classList.add('clip'); 
 	this.sound.style.display = 'none';
 	document.body.appendChild(this.sound);
 	this.play = function() {
